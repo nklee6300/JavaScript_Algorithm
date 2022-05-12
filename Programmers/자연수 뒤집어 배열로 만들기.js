92a5dd4d387b94b99cc04e36aa449d62766afa85
nklee6300 <nogi3322@naver.com>
2022-05-12 16:36:25 +0900

    2022-05-12

--- Programmers/자연수 뒤집어 배열로 만들기.js
+++ Programmers/자연수 뒤집어 배열로 만들기.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let answer = []
     let temp = String(n).split('').reverse()
- 
+
     for (let i = 0; i < temp.length; i++) {
         answer.push(parseInt(temp[i]))
     }
